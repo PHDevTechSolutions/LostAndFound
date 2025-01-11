d49fe88dcd8e32fba552fbb897a36d2281be8574
ERPReactNextDev <it.ecoshiftcorp@gmail.com>
2025-01-11 05:09:00 +0000

    Automated commit

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Magdagdag ng lahat ng pagbabago sa git
+git add .
+
+# I-commit ang mga pagbabago
+git commit -m "Automated commit"
+
+# I-push ang mga pagbabago sa main branch
+git push origin main
