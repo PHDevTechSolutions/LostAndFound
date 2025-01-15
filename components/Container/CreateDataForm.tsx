d84cae70bd45c945d2875a204f2ed33983147bc8
PHDevTechSolutions <phdevtechsolutions@gmail.com>
2025-01-16 01:16:56 +0800

    Update CreateDataForm.tsx

--- components/Container/CreateDataForm.tsx
+++ components/Container/CreateDataForm.tsx
@@ -141,7 +141,6 @@ const CreateDataForm: React.FC<CreateDataFormProps> = ({ post, onCancel }) => {
                 <h2 className="text-xs font-semibold text-gray-700">
                     {post?.Vendor}
                 </h2>
-<h3>{userName}</h3>
                 <h2 className="text-xs font-semibold text-gray-700 mb-6">
                     Container Van No. {post?.ContainerNo}
                 </h2>
@@ -203,7 +202,12 @@ const CreateDataForm: React.FC<CreateDataFormProps> = ({ post, onCancel }) => {
                         </div>
                         <div className="mb-4">
                             <label className="block text-xs font-bold mb-2" htmlFor="PaymentMode">Mode of Payment</label>
-                            <input type="text" id="PaymentMode" value={PaymentMode} onChange={(e) => setPaymentMode(e.target.value)} className="w-full px-3 py-2 border rounded text-xs" required />
+                         
+<select id="PaymentMode" value={PaymentMode} onChange={(e) => setPaymentMode(e.target.value)} className="w-full px-3 py-2 border rounded text-xs" required >
+<option value="">Select Mode</option>
+<option value="Cash">Cash</option>
+<option value="PDC">PDC</option>
+</select>
                         </div>
                         <div className="flex justify-between">
                             <button type="button" onClick={onCancel} className="text-xs text-white bg-gray-400 hover:bg-gray-500 px-4 py-2 rounded-md">Cancel</button>
