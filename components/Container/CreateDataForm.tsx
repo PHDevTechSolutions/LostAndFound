b896f263c76aaf29940815534a38ac675d99f435
PHDevTechSolutions <phdevtechsolutions@gmail.com>
2025-01-17 06:01:18 +0000

    Initial Commit

--- components/Container/CreateDataForm.tsx
+++ components/Container/CreateDataForm.tsx
@@ -369,7 +369,7 @@ const { totalBoxSales, totalPrice, totalRemaining, totalGrossSales } = calculate
         <td colSpan={2} className="text-xs font-semibold text-right border px-4 py-2">Total</td>
         <td className="text-xs font-semibold border px-4 py-2">{totalBoxSales}</td>
         <td className="text-xs font-semibold border px-4 py-2">₱{new Intl.NumberFormat('en-PH', { minimumFractionDigits: 2 }).format(totalPrice)}</td>
-        <td className="text-xs font-semibold border px-4 py-2">₱{new Intl.NumberFormat('en-PH', { minimumFractionDigits: 2 }).format(totalRemaining)}</td>
+        <td className="text-xs font-semibold border px-4 py-2">{(totalRemaining)}</td>
         <td className="text-xs font-semibold border px-4 py-2">₱{new Intl.NumberFormat('en-PH', { minimumFractionDigits: 2 }).format(totalGrossSales)}</td>
         <td colSpan={3}></td>
     </tr>
