51682f8e7d5cadfb392e0a44dde8b3a52129dd63
PHDevTechSolutions <phdevtechsolutions@gmail.com>
2025-01-22 16:58:30 +0800

    Update FetchUser.ts

--- pages/api/User/FetchUser.ts
+++ pages/api/User/FetchUser.ts
@@ -1,7 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { connectToDatabase } from '../../../lib/mongodb';
 
-export default async function UsersAccounts(req: NextApiRequest, res: NextApiResponse) {
+export default async function fetchAccounts(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'GET') {
     res.setHeader('Allow', ['GET']);
     res.status(405).end(`Method ${req.method} Not Allowed`);
@@ -10,11 +10,11 @@ export default async function UsersAccounts(req: NextApiRequest, res: NextApiRes
 
   try {
     const db = await connectToDatabase();
-    const accountCollection = db.collection('users');
-    const accounts = await accountCollection.find({}).toArray();
-    res.status(200).json(accounts);
+    const UserCollection = db.collection('users');
+    const data = await UserCollection.find({}).toArray();
+    res.status(200).json(data);
   } catch (error) {
-    console.error('Error fetching accounts:', error);
-    res.status(500).json({ error: 'Failed to fetch accounts' });
+    console.error('Error fetching data:', error);
+    res.status(500).json({ error: 'Failed to fetch data' });
   }
 }
