f496eda0262b4428b3ff30751bc4bcf47e38aa1e
PHDevTechSolutions <phdevtechsolutions@gmail.com>
2025-01-22 06:46:24 +0000

    Babyrose

--- components/User/UserFields.tsx
+++ components/User/UserFields.tsx
@@ -21,33 +21,39 @@ interface UserFieldsProps{
 const UserFields: React.FC<UserFieldsProps> = ({ Firstname, setFirstname, Lastname, setLastname, Email, setEmail, Location, setLocation, UserName, setUserName, Password, setPassword, Role, setRole }) => {
     return (
         <>
-        <div className="mb-4">
-            <label className="block text-xs font-bold mb-2" htmlFor="firstname">Firstname</label>
+        <div className="flex flex-wrap -mx-4">
+            <label className="block text-xs font-bold mb-2" htmlFor="Firstname">Firstname</label>
             <input type="text" id="Firstname" value={Firstname} onChange={(e) => setFirstname(e.target.value)} className="w-full px-3 py-2 border rounded text-xs" required/>
         </div> 
-        <div className="mb-4">
-            <label className="block text-xs font-bold mb-2" htmlFor="lastname">Lastname</label>
+        <div className="flex flex-wrap -mx-4">
+            <label className="block text-xs font-bold mb-2" htmlFor="Lastname">Lastname</label>
             <input type="text" id="Lastname" value={Lastname} onChange={(e) => setLastname(e.target.value)} className="w-full px-3 py-2 border rounded text-xs" required/>
         </div> 
-        <div className="mb-4">
-            <label className="block text-xs font-bold mb-2" htmlFor="email">Email</label>
+        <div className="flex flex-wrap -mx-4">
+            <label className="block text-xs font-bold mb-2" htmlFor="Email">Email</label>
             <input type="text" id="Email" value={Email} onChange={(e) => setEmail(e.target.value)} className="w-full px-3 py-2 border rounded text-xs" required/>
         </div> 
-        <div className="mb-4">
-            <label className="block text-xs font-bold mb-2" htmlFor="location">Location</label>
-            <input type="text" id="Location" value={Location} onChange={(e) => setLocation(e.target.value)} className="w-full px-3 py-2 border rounded text-xs" required/>
+        <div className="flex flex-wrap -mx-4">
+            <label className="block text-xs font-bold mb-2" htmlFor="Location">Location</label>
+            <select id="Location" value={Location || ""} onChange={(e) => setLocation(e.target.value)} className="w-full px-3 py-2 border rounded text-xs" required >
+            <option value="Navotas">Navotas</option>
+            <option value="Sambat">Sambat</option>
+            <option value="Minalin">Minalin</option>
+          </select>
         </div> 
-        <div className="mb-4">
-            <label className="block text-xs font-bold mb-2" htmlFor="username">Username</label>
+        <div className="flex flex-wrap -mx-4">
+            <label className="block text-xs font-bold mb-2" htmlFor="Username">Username</label>
             <input type="text" id="UserName" value={UserName} onChange={(e) => setUserName(e.target.value)} className="w-full px-3 py-2 border rounded text-xs" required/>
         </div> 
-        <div className="mb-4">
-            <label className="block text-xs font-bold mb-2" htmlFor="password">Password</label>
+        <div className="flex flex-wrap -mx-4">
+            <label className="block text-xs font-bold mb-2" htmlFor="Password">Password</label>
             <input type="text" id="Password" value={Password} onChange={(e) => setPassword(e.target.value)} className="w-full px-3 py-2 border rounded text-xs" required/>
         </div> 
-        <div className="mb-4">
-            <label className="block text-xs font-bold mb-2" htmlFor="role">Role</label>
-            <input type="text" id="Role" value={Role} onChange={(e) => setRole(e.target.value)} className="w-full px-3 py-2 border rounded text-xs" required/>
+        <div className="flex flex-wrap -mx-4">
+            <label className="block text-xs font-bold mb-2" htmlFor="Role">Role</label>
+            <select id="Role" value={Role || ""} onChange={(e) => setRole(e.target.value)} className="w-full px-3 py-2 border rounded text-xs" required >
+            <option value="Admin">Admin</option>
+          </select>
         </div> 
         </>
     )
