f918a0066a272069be5084f1a7c38f1930d791d5
PHDevTechSolutions <phdevtechsolutions@gmail.com>
2025-01-22 17:25:01 +0800

    Update page.tsx

--- app/User/ListofUser/page.tsx
+++ app/User/ListofUser/page.tsx
@@ -7,135 +7,154 @@ import UserFetcher from "../../../components/UserFetcher";
 import AddUserForm from "../../../components/User/AddUserForm";
 import UsersTable from "../../../components/User/UsersTable";
 import { ToastContainer, toast } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
-
+import "react-toastify/dist/ReactToastify.css";
 
 const ListofUser: React.FC = () => {
-    const [showForm, setShowForm] = useState(false);
-    const [editPost, setEditPost] = useState<any>(null);
-    const [posts, setPosts] = useState<any[]>([]);
-    const [searchTerm, setSearchTerm] = useState("");
-    const [selectedCityAddress, setSelectedCityAddress] = useState("");
-    const [currentPage, setCurrentPage] = useState(1);
-    const [postsPerPage, setPostsPerPage] = useState(5);
-    const [showDeleteModal, setShowDeleteModal] = useState(false);
-    const [postToDelete, setPostToDelete] = useState<string | null>(null);
+  const [showForm, setShowForm] = useState(false);
+  const [editPost, setEditPost] = useState<any>(null);
+  const [posts, setPosts] = useState<any[]>([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [selectedCityAddress, setSelectedCityAddress] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostsPerPage] = useState(5);
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
+  const [postToDelete, setPostToDelete] = useState<string | null>(null);
 
-    const UsersAccounts = async () => {
-        try {
-            const response = await fetch("/api/User/FetchUser");
-            const data = await response.json();
-            setPosts(data);
-        } catch (error) {
-            toast.error("Error Fetch");
-            console.error("Error Fetching", error);
-        }
-    };
+  const UsersAccounts = async () => {
+    try {
+      const response = await fetch("/api/User/FetchUser");
+      if (!response.ok) throw new Error("Failed to fetch users.");
+      const data = await response.json();
+      setPosts(data);
+    } catch (error) {
+      toast.error("Error fetching users.");
+      console.error("Error Fetching", error);
+    }
+  };
 
-    const filteredAccounts = posts.filter((post) => {
-        return (
-            (post.UserName.toLowerCase().includes(searchTerm.toLowerCase()) ||
-                post.Email.toLowerCase().includes(searchTerm.toLowerCase()) ));
-    });
-
-    // Pagination logic
-    const indexOfLastPost = currentPage * postsPerPage;
-    const indexOfFirstPost = indexOfLastPost - postsPerPage;
-    const currentPosts = filteredAccounts.slice(indexOfFirstPost, indexOfLastPost);
-    const totalPages = Math.ceil(filteredAccounts.length / postsPerPage);
-
-    useEffect(() => {
-        UsersAccounts();
-    }, []);
-
-    const handleEdit = (post: any) => {
-        setEditPost(post);
-        setShowForm(true);
-    };
-
-    const confirmDelete = (postId: string) => {
-        setPostToDelete(postId);
-        setShowDeleteModal(true);
-    };
-
-    const handleDelete = async () => {
-        if (!postToDelete) return;
-        try {
-            const response = await fetch(`/api/account/deleteAccount`, {
-                method: "DELETE",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({ id: postToDelete }),
-            });
-
-            if (response.ok) {
-                setPosts(posts.filter((post) => post._id !== postToDelete));
-                toast.success("Account deleted successfully.");
-            } else {
-                toast.error("Failed to delete account.");
-            }
-        } catch (error) {
-            toast.error("Failed to delete account.");
-            console.error("Error deleting account:", error);
-        } finally {
-            setShowDeleteModal(false);
-            setPostToDelete(null);
-        }
-    };
-
+  const filteredAccounts = posts.filter((post) => {
     return (
-        <SessionChecker>
-            <ParentLayout>
-                <UserFetcher>
-                    {(userName) => (
-                        <div className="container mx-auto p-4">
-                            <div className="grid grid-cols-1 md:grid-cols-1">
-                                {showForm ? (
-                                    <AddUserForm
-                                        onCancel={() => {
-                                            setShowForm(false);
-                                        }}
-                                        // Pass the refreshPosts callback
-                                        userName={userName}
-
-                                    />
-                                ) : (
-                                    <>
-                                        <div className="flex justify-between items-center mb-4">
-                                            <button className="bg-blue-800 text-white px-4 text-xs py-2 rounded" onClick={() => setShowForm(true)}>Create User</button>
-                                        </div>
-
-                                        <div className="mb-4 p-4 bg-white shadow-md rounded-lg ">
-                                            <UsersTable
-                                                posts={currentPosts}
-                                                handleEdit={handleEdit}
-                                                handleDelete={confirmDelete}
-                                            />
-                                        </div>
-                                    </>
-                                )}
-
-                                {showDeleteModal && (
-                                    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-                                        <div className="bg-white p-4 rounded shadow-lg">
-                                            <h2 className="text-xs font-bold mb-4">Confirm Deletion</h2>
-                                            <p className="text-xs">Are you sure you want to delete this account?</p>
-                                            <div className="mt-4 flex justify-end">
-                                                <button className="bg-red-500 text-white text-xs px-4 py-2 rounded mr-2" onClick={handleDelete}>Delete</button>
-                                                <button className="bg-gray-300 text-xs px-4 py-2 rounded" onClick={() => setShowDeleteModal(false)}>Cancel</button>
-                                            </div>
-                                        </div>
-                                    </div>
-                                )}
-
-                            </div>
-                        </div>
-                    )}
-                </UserFetcher>
-            </ParentLayout>
-        </SessionChecker>
+      (post?.UserName?.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        post?.Email?.toLowerCase().includes(searchTerm.toLowerCase()))
     );
+  });
+
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = filteredAccounts.slice(indexOfFirstPost, indexOfLastPost);
+  const totalPages = Math.ceil(filteredAccounts.length / postsPerPage);
+
+  useEffect(() => {
+    UsersAccounts();
+  }, []);
+
+  const handleEdit = (post: any) => {
+    setEditPost(post);
+    setShowForm(true);
+  };
+
+  const confirmDelete = (postId: string) => {
+    setPostToDelete(postId);
+    setShowDeleteModal(true);
+  };
+
+  const handleDelete = async () => {
+    if (!postToDelete) return;
+    try {
+      const response = await fetch(`/api/account/deleteAccount`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ id: postToDelete }),
+      });
+
+      if (response.ok) {
+        setPosts(posts.filter((post) => post._id !== postToDelete));
+        toast.success("Account deleted successfully.");
+      } else {
+        toast.error("Failed to delete account.");
+      }
+    } catch (error) {
+      toast.error("Failed to delete account.");
+      console.error("Error deleting account:", error);
+    } finally {
+      setShowDeleteModal(false);
+      setPostToDelete(null);
+    }
+  };
+
+  return (
+    <SessionChecker>
+      <ParentLayout>
+        <UserFetcher>
+          {(userName) => (
+            <div className="container mx-auto p-4">
+              <ToastContainer />
+              <div className="grid grid-cols-1 md:grid-cols-1">
+                {showForm ? (
+                  <AddUserForm
+                    onCancel={() => {
+                      setShowForm(false);
+                    }}
+                    userName={userName}
+                  />
+                ) : (
+                  <>
+                    <div className="flex justify-between items-center mb-4">
+                      <button
+                        className="bg-blue-800 text-white px-4 text-xs py-2 rounded"
+                        onClick={() => setShowForm(true)}
+                      >
+                        Create User
+                      </button>
+                    </div>
+
+                    <div className="mb-4 p-4 bg-white shadow-md rounded-lg ">
+                      {currentPosts.length > 0 ? (
+                        <UsersTable
+                          posts={currentPosts}
+                          handleEdit={handleEdit}
+                          handleDelete={confirmDelete}
+                        />
+                      ) : (
+                        <p>No users found.</p>
+                      )}
+                    </div>
+                  </>
+                )}
+
+                {showDeleteModal && (
+                  <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+                    <div className="bg-white p-4 rounded shadow-lg">
+                      <h2 className="text-xs font-bold mb-4">Confirm Deletion</h2>
+                      <p className="text-xs">
+                        Are you sure you want to delete this account?
+                      </p>
+                      <div className="mt-4 flex justify-end">
+                        <button
+                          className="bg-red-500 text-white text-xs px-4 py-2 rounded mr-2"
+                          onClick={handleDelete}
+                        >
+                          Delete
+                        </button>
+                        <button
+                          className="bg-gray-300 text-xs px-4 py-2 rounded"
+                          onClick={() => setShowDeleteModal(false)}
+                        >
+                          Cancel
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                )}
+              </div>
+            </div>
+          )}
+        </UserFetcher>
+      </ParentLayout>
+    </SessionChecker>
+  );
 };
 
 export default ListofUser;
