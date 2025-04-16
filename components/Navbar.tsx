1c92193bcf6a7788f2ef89292a78deaae494865c
PHDevTechSolutions <phdevtechsolutions@gmail.com>
2025-04-16 14:15:56 +0800

    Leroux Y Xchire

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -53,7 +53,7 @@ const Navbar: React.FC<{ onToggleSidebar: () => void }> = ({ onToggleSidebar }) 
       {showLogoutModal && (
         <div className="fixed inset-0 bg-gray-600 bg-opacity-50 flex justify-center items-center z-50">
           <div className="bg-white p-6 rounded-lg shadow-lg text-center text-gray-800">
-            <p className="mb-4 text-xs">Are you sure you want to logout?</p>
+            <p className="mb-4 text-xs">Are you sure you want to logout?.</p>
             <div className="flex justify-center space-x-4">
               <button className="bg-gray-300 px-4 py-2 rounded text-gray-800 text-xs" onClick={() => setShowLogoutModal(false)}>
                 Cancel
